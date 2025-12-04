846bd8b19eb0d127d52dbd1518eeba3469640fd4
Samira Dorchalian <samiradorchalain1998@gmail.com>
2025-12-04 20:39:08 +0330

    switch to sqlite + render.yaml

--- config/settings.py
+++ config/settings.py
@@ -81,16 +81,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'bookapp',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'samira1998',
+#         # --------------
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookapp',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'samira1998',
-        # --------------
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
